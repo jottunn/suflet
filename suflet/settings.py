8cfbd06229d35ce3c01c1aca55cd1371fb006e2b
jottunn <luci_u25@yahoo.com>
2023-06-26 16:07:43 +0300

    -djnago 4.x compatibility changes

--- suflet/settings.py
+++ suflet/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = [
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '0.0.0.0']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.{}'.format(os.getenv('LOAD_BALANCER_IP', None)), 'https://*.127.0.0.1']
 
 # Application definition
 ROOT_URLCONF     = 'suflet.urls'
